5fcb3a79632a8ac0288fcef7c3bd40f152830832
m1gue21 <miguefreeagent@gmail.com>
2023-07-11 16:47:28 -0500

    add crud operations

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,29 +1,58 @@
 from chalice import Chalice
+import json
 
 app = Chalice(app_name='helloworld')
 
+USERS = {}
 
-@app.route('/')
+
+# Example user in USERS:
+# {
+#     "some_username123": {
+#         "username": "some_username123",
+#         "age": 20
+#     }
+# }
+
+@app.route("/")
 def index():
     return {'hello': 'world'}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+# CREATE (POST)
+@app.route('/users', methods=['POST'])
+def create_user():
+    user_as_json = app.current_request.json_body
+    USERS[user_as_json["username"]] = user_as_json
+    return user_as_json
+
+
+# READ (GET)
+@app.route("/users", methods=["GET"])
+def read_users():
+    return json.dumps(USERS)
+
+
+# READ (GET)
+@app.route("/users/{username}", methods=["GET"])
+def read_user(username):
+    if username and username in USERS:
+        return json.dumps(USERS[username])
+    return {"error": f"'{username}' is not an existing user."}
+
+
+# UPDATE (PUT)
+@app.route("/users", methods=["PUT"])
+def update_user():
+    user_as_json = app.current_request.json_body
+    USERS[user_as_json["username"]] = user_as_json
+    return user_as_json
+
+
+# DELETE (DELETE)
+@app.route("/users", methods=["DELETE"])
+def delete_user():
+    user_as_json = app.current_request.json_body
+    if user_as_json["username"] in USERS:
+        del USERS[user_as_json["username"]]
+    return user_as_json
