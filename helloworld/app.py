40be690fd843fc756eaa1fd87f27bb9c47650e04
m1gue21 <miguefreeagent@gmail.com>
2023-07-11 21:00:07 -0500

    add pynamodb

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,58 +1,80 @@
 from chalice import Chalice
 import json
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute
 
 app = Chalice(app_name='helloworld')
 
-USERS = {}
+
+# Define user model
+class User(Model):
+    class Meta:
+        table_name = 'users_table'
+        region = 'us-east-1'
+
+    username = UnicodeAttribute(hash_key=True)
+    age = NumberAttribute()
 
 
-# Example user in USERS:
-# {
-#     "some_username123": {
-#         "username": "some_username123",
-#         "age": 20
-#     }
-# }
-
-@app.route("/")
-def index():
-    return {'hello': 'world'}
+if not User.exists():
+    User.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
 
 
 # CREATE (POST)
 @app.route('/users', methods=['POST'])
 def create_user():
     user_as_json = app.current_request.json_body
-    USERS[user_as_json["username"]] = user_as_json
+    user = User(username=user_as_json["username"], age=user_as_json["age"])
+    user.save()
     return user_as_json
 
 
 # READ (GET)
 @app.route("/users", methods=["GET"])
 def read_users():
-    return json.dumps(USERS)
+    users = User.scan()
+    user_data = []
+    for user in users:
+        user_data.append({
+            "username": user.username,
+            "age": user.age
+        })
+    return json.dumps(user_data)
 
 
 # READ (GET)
 @app.route("/users/{username}", methods=["GET"])
 def read_user(username):
-    if username and username in USERS:
-        return json.dumps(USERS[username])
-    return {"error": f"'{username}' is not an existing user."}
+    try:
+        user = User.get(username)
+        return {
+            "username": user.username,
+            "age": user.age
+        }
+    except User.DoesNotExist:
+        return {"error": f"'{username}' is not an existing user."}
 
 
 # UPDATE (PUT)
 @app.route("/users", methods=["PUT"])
 def update_user():
     user_as_json = app.current_request.json_body
-    USERS[user_as_json["username"]] = user_as_json
-    return user_as_json
+    try:
+        user = User.get(user_as_json["username"])
+        user.age = user_as_json["age"]
+        user.save()  # update user
+        return user_as_json
+    except User.DoesNotExist:
+        return {"error": f"'{user_as_json['username']}' is not an existing user."}
 
 
 # DELETE (DELETE)
 @app.route("/users", methods=["DELETE"])
 def delete_user():
     user_as_json = app.current_request.json_body
-    if user_as_json["username"] in USERS:
-        del USERS[user_as_json["username"]]
-    return user_as_json
+    try:
+        user = User.get(user_as_json["username"])
+        user.delete()  # deletes user
+        return user_as_json
+    except User.DoesNotExist:
+        return {"error": f"'{user_as_json['username']}' is not an existing user."}
